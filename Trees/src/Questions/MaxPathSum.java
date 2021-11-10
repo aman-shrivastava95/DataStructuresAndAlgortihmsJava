50d3c150248a4c92cb249e9e272a97f6146c6e43
Aman Shrivastava <amanshri.nitrr@gmail.com>
2021-11-10 20:13:07 +0530

    Changed the method name 'helper' to 'maxPathHelper'.

--- Trees/src/Questions/MaxPathSum.java
+++ Trees/src/Questions/MaxPathSum.java
@@ -5,15 +5,15 @@ import Nodes.local.IntNode;
 public class MaxPathSum{
     int result = Integer.MIN_VALUE ;
     public int solve(IntNode root){
-        helper(root) ;
+        maxPathHelper(root) ;
         return result ;
     }
     /*Basically we try to find the maximum of all the three cases, it could be straight, or it coult start from a node, or contains both side of the subtree*/
     /*try to visualize it, it is pretty easy*/
-    private int helper(IntNode node) {
+    private int maxPathHelper(IntNode node) {
         if(node == null) return 0 ;
-        int L = helper(node.left) ;
-        int R = helper(node.right) ;
+        int L = maxPathHelper(node.left) ;
+        int R = maxPathHelper(node.right) ;
         int straight  = Math.max(node.val, node.val + Math.max(L,R)) ;
         int containsBothLandR = Math.max(straight,L + R + node.val) ;
         result = Math.max(result,containsBothLandR) ;
