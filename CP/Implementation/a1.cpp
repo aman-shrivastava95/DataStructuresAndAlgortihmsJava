bf366372a6df68353d98c30983966f16b87acc27
Aman Shrivastava <ashrivastava@microsoft.com>
2022-02-14 20:02:09 +0530

    setup for CP

---
+++ CP/Implementation/a1.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>//
+#define pb push_back
+#define mp make_pair
+#define MOD  100000007 
+#define len(x) x.size()
+#define min3(a,b,c),min(a, min(b,c))
+#define max3(a,b,c), max(a, max(b,c))
+#define all(v) v.begin(),v.end() 
+#define alla(n) a,a + n
+using namespace std ;
+
+int main(int argc, char const *argv[])
+{
+    
+    #ifndef ONLINE_JUDGE
+    freopen("../input.txt","r",stdin) ;
+    freopen("../output.txt","w",stdout) ;
+    #endif
+    ios_base::sync_with_stdio(false) ;
+    cin.tie(NULL) ; cout.tie(NULL) ;
+    int n ;
+    cin>>n;
+    int x = 0 ;
+    while(n--){
+        string s ;
+        cin>>s ;
+        if(s[0]=='+' || s[s.size()-1]=='+')
+            x++ ;
+        else
+            x-- ;
+    }
+    cout<<x ;
+    
+}
---
+++ Tools_and_stl/playingWithStrings.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define MOD  100000007 
+#define len(x) x.size()
+#define min3(a,b,c),min(a, min(b,c))
+#define max3(a,b,c), max(a, max(b,c))
+#define all(v) v.begin(),v.end() 
+#define alla(n) a,a + n
+using namespace std ;
+
+//this is awesome
+int main(int argc, char const *argv[])
+{
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin) ;
+    freopen("output.txt","w",stdout) ;
+    #endif
+    ios_base::sync_with_stdio(false) ;
+    cin.tie(NULL) ; cout.tie(NULL) ;
+
+    pair<int, int> P = mp(1,2) ;
+    cout<<P.first<< " "<<P.second ;
+    vector<int> v ;
+    v.pb(79) ;
+    v.pb(89) ;
+    cout<<len(v) ;
+
+}
