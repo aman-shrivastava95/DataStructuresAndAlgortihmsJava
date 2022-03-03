e5a4deb11c791c9c23deaadc655440f5eccd78a4
Aman Shrivastava <ashrivastava@microsoft.com>
2022-03-03 12:44:46 +0530

    printing LCS

--- DynamicProgramming/src/longestCommonSequence.cpp
+++ DynamicProgramming/src/longestCommonSequence.cpp
@@ -56,6 +56,28 @@ int lcsTab(string s, string t){
                     dp[i][j] = max(dp[i-1][j],dp[i][j-1]) ;
         }
     }
+    //printing the LCS
+    int len = dp[n][m] ;
+    string ans = "" ;
+    for(int i=0;i<len;i++){
+        ans+='$' ;
+    }
+    int index = len-1; 
+    int i=n,j=m ;
+    while(i>0 && j>0){
+        if(s[i-1]==t[j-1]){
+            ans[index] = s[i-1] ;
+            index-- ;
+            i--;
+            j-- ;
+        }else if(dp[i-1][j] > dp[i][j-1]){
+            i-- ;
+        }else{
+            j-- ;
+        }
+    }
+    cout<<ans<<endl;
+    
     return dp[n][m] ;
 }
 
