b3032b0c57d6c680ea40a88d3c75866a169f2e21
aman <amanshri.nitrr@gmail.com>
2022-04-05 01:16:20 +0530

    change

--- DynamicProgramming/src/cherries.cpp
+++ DynamicProgramming/src/cherries.cpp
@@ -7,7 +7,7 @@ int cherries(int i, int j1, int j2, int r, int c, vector<vector<int>> &grid,vect
     if(j1 <0 || j2 < 0 || j1 > c-1 || j2 > c-1)
         return -1e9 ;
 
-    //base case, reached last column    
+    //base case, reached last column,    
     if(i == r-1){
         if(j1==j2) return grid[i][j1] ;
         else return grid[i][j1] + grid[i][j2] ;
