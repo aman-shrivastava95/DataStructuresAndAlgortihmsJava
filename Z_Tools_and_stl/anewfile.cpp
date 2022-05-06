f3a6349921d9ce862dff4e77840c4f6a1cbb5b88
Aman <amanshri.nitrr@gmail.com>
2022-05-06 13:18:34 +0530

    rate limiter link

--- Z_Tools_and_stl/anewfile.cpp
+++ Z_Tools_and_stl/anewfile.cpp
@@ -6,3 +6,6 @@ int main(int argc, char const *argv[])
     cout<<"demo file to add in the git" <<endl ;
     return 0;
 }
+
+//Rate limiter
+//https://hechao.li/2018/06/25/Rate-Limiter-Part1/#:~:text=Sliding%20window%20counter%20is%20similar,01%2C%2000%3A02
